6fd1256f71277970371d99d0b6ae5c41b9a9b9a6
ohjh9701 <ohjh4875@gmail.com>
2026-01-27 19:09:37 +0900

    [수정] 오류 확인을 위한 수정

--- springJDBCBoard/src/main/java/com/board/controller/UsersController.java
+++ springJDBCBoard/src/main/java/com/board/controller/UsersController.java
@@ -1,4 +1,4 @@
-package com.users.controller;
+package com.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Slf4j
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UsersController {
 	
 	@GetMapping("/insertForm")
-	public String usersInsertForm(Model model) {
+	public String getMethodName(Model model) {
 		return "users/insertForm";
 	}
 	
