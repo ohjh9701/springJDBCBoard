72b15cd0f92bc30ccc214b5427bfae81d9edf403
ohjh9701 <ohjh4875@gmail.com>
2026-01-27 17:49:59 +0900

    [생성] 게시판 기능에서 입력, 리스트 출력, 상세페이지 구현 실습 완료

---
+++ springJDBCBoard/src/main/java/com/board/domain/Board.java
@@ -0,0 +1,15 @@
+package com.board.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Board {
+	
+	private int no;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regDate;
+}
