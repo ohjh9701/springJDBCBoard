4b4607185481cdf7b4ada37a6a077bcdbeaa1b58
ohjh9701 <ohjh4875@gmail.com>
2026-01-27 14:44:56 +0900

    [생성] 데이터베이스 연동 실습을 위한 게시판 프로젝트 생성

---
+++ springJDBCBoard/src/main/java/com/board/controller/BoardController.java
@@ -0,0 +1,23 @@
+package com.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Slf4j
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	
+	@GetMapping("/insertForm")
+	public String boardInsertForm(Model model) {
+		return "board/insertForm";
+	}
+	
+	
+}
